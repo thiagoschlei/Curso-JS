9e9bac816116d2fbd2e0ec352a3f937b49d9879c
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-04-26 21:31:28 -0300

    Adicionei aula 15 - Vetores

---
+++ Aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5,8,4]
+num[3] = 6
+num.push(7)
+
+console.log(num)
+console.log("O vetor num é composto dos seguintes números: "+ num)
+console.log("O vetor num possui "+ num.length +" elementos.")
+console.log("O quarto elemento do vetor é o "+ num[3])
+console.log("Em ordem crescente, o vetor fica da seguinte maneira: " +num.sort())
